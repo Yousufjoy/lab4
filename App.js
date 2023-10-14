b2422b1565acbfbb7c3f5e84a53bb248624886d2
student <sadabuzzaman75@gmail.com>
2023-10-14 08:52:21 +0600

    sdfsd

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Opesdfn up aaApp.js to start working on your app!</Text>
+      <Text>Opesdfn up aaApp.js twqeo start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
